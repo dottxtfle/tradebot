bb8e8b30bab8ba2f01f0d26571977128d48404a0
dottxtfle <104865180+dottxtfle@users.noreply.github.com>
2022-05-04 08:33:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,46 @@
-import roblox, asyncio, json, re
-
-async def best_price(e):
-    price = 0
-    for item in e:
-        id = int(''.join(re.findall(r'\b\d+\b', item['ItemLink'])))
-        resellers = await roblox.resellers(id)
-        if resellers.status_code == 200:
-            resellers_json = resellers.json()
-            price += resellers_json['data'][0]['price']
-    return price
-
-
-def parse_items(e):
-    str = ''
-    for item in e:
-        str += item['Name'] + ', '
-    return str
-
-
-async def run():
-    trades = await roblox.get_trades()
-    if trades.status_code != 200:
-        print('Your cookie has expired...')
-        exit()
-    data = json.loads(json.loads(trades.text)['d'])['Data']
-    print(f'Found {len(data)} trades...\n')
-    for i in range(len(data)):
-        print(f'checking trade #{i+1}')
-        trade = data[i]
-        trade_identify = json.loads(trade)
-        trade_object = await roblox.execute_trade(int(trade_identify['TradeSessionID']), 'pull')
-        if trade_object.status_code != 200:
-            print(f'Got status code {trade_object.status_code} when trying to fetch trade...')
-            continue
-        trade_json = json.loads(trade_object.json()['data'])['AgentOfferList']
-        print(f'trade #{i+1} offer value is {trade_json[0]["OfferValue"]}')
-        print(f'trade #{i+1} request value is {trade_json[1]["OfferValue"]}')
-        prices = [await best_price(trade_json[0]['OfferList']), await best_price(trade_json[1]['OfferList'])]
-        if prices[0] < prices[1]:
-            print(f':pensive: trade #{i+1} is bad you will lose {prices[0] - prices[1]} robux')
-            continue
-        else:
-            msg = input(f'\nTrade #{i+1}:\nYou will win {prices[0]-prices[1]} robux\nYou will give: {parse_items(trade_json[1]["OfferList"])}\nYou will get: {parse_items(trade_json[0]["OfferList"])}\nAccept Trade? (y/n) :')
-            if msg.lower().startswith('y'):
-                await roblox.execute_trade(int(trade_identify['TradeSessionID']), 'accept')
-            else:
-                await roblox.execute_trade(int(trade_identify['TradeSessionID']), 'decline')
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
-loop.close()
+import roblox ,asyncio ,json ,re #line:1:import roblox, asyncio, json, re
+async def best_price (OO0OOO00OO0OO0O0O ):#line:3:async def best_price(e):
+    OOOO0OOO0O00OO000 =0 #line:4:price = 0
+    for OOOO00OOOOOOO00OO in OO0OOO00OO0OO0O0O :#line:5:for item in e:
+        OOOOOOOO00O0O0000 =int (''.join (re .findall (r'\b\d+\b',OOOO00OOOOOOO00OO ['ItemLink'])))#line:6:id = int(''.join(re.findall(r'\b\d+\b', item['ItemLink'])))
+        OOOOO0OOOO00OOO00 =await roblox .resellers (OOOOOOOO00O0O0000 )#line:7:resellers = await roblox.resellers(id)
+        if OOOOO0OOOO00OOO00 .status_code ==200 :#line:8:if resellers.status_code == 200:
+            OO000OO000O0OOO0O =OOOOO0OOOO00OOO00 .json ()#line:9:resellers_json = resellers.json()
+            OOOO0OOO0O00OO000 +=OO000OO000O0OOO0O ['data'][0 ]['price']#line:10:price += resellers_json['data'][0]['price']
+    return OOOO0OOO0O00OO000 #line:11:return price
+def parse_items (OO0O000OO00OO0OOO ):#line:14:def parse_items(e):
+    OOOO000OO0000O00O =''#line:15:str = ''
+    for O00OOOOOO0OO0O0OO in OO0O000OO00OO0OOO :#line:16:for item in e:
+        OOOO000OO0000O00O +=O00OOOOOO0OO0O0OO ['Name']+', '#line:17:str += item['Name'] + ', '
+    return OOOO000OO0000O00O #line:18:return str
+async def run ():#line:21:async def run():
+    O0O00OOOOOO0O0OO0 =await roblox .get_trades ()#line:22:trades = await roblox.get_trades()
+    if O0O00OOOOOO0O0OO0 .status_code !=200 :#line:23:if trades.status_code != 200:
+        print ('Your cookie has expired...')#line:24:print('Your cookie has expired...')
+        exit ()#line:25:exit()
+    O0O00O0000OO00000 =json .loads (json .loads (O0O00OOOOOO0O0OO0 .text )['d'])['Data']#line:26:data = json.loads(json.loads(trades.text)['d'])['Data']
+    print (f'Found {len(O0O00O0000OO00000)} trades...\n')#line:27:print(f'Found {len(data)} trades...\n')
+    for O0O0000OOO000OOOO in range (len (O0O00O0000OO00000 )):#line:28:for i in range(len(data)):
+        print (f'checking trade #{O0O0000OOO000OOOO+1}')#line:29:print(f'checking trade #{i+1}')
+        O00OOO0O0O000O0O0 =O0O00O0000OO00000 [O0O0000OOO000OOOO ]#line:30:trade = data[i]
+        OOOOO000O0O000O00 =json .loads (O00OOO0O0O000O0O0 )#line:31:trade_identify = json.loads(trade)
+        O0OOO0O00O000O00O =await roblox .execute_trade (int (OOOOO000O0O000O00 ['TradeSessionID']),'pull')#line:32:trade_object = await roblox.execute_trade(int(trade_identify['TradeSessionID']), 'pull')
+        if O0OOO0O00O000O00O .status_code !=200 :#line:33:if trade_object.status_code != 200:
+            print (f'Got status code {O0OOO0O00O000O00O.status_code} when trying to fetch trade...')#line:34:print(f'Got status code {trade_object.status_code} when trying to fetch trade...')
+            continue #line:35:continue
+        O00O0OOO00000O0OO =json .loads (O0OOO0O00O000O00O .json ()['data'])['AgentOfferList']#line:36:trade_json = json.loads(trade_object.json()['data'])['AgentOfferList']
+        print (f'trade #{O0O0000OOO000OOOO+1} offer value is {O00O0OOO00000O0OO[0]["OfferValue"]}')#line:37:print(f'trade #{i+1} offer value is {trade_json[0]["OfferValue"]}')
+        print (f'trade #{O0O0000OOO000OOOO+1} request value is {O00O0OOO00000O0OO[1]["OfferValue"]}')#line:38:print(f'trade #{i+1} request value is {trade_json[1]["OfferValue"]}')
+        OOOOOOOOOOO00OOO0 =[await best_price (O00O0OOO00000O0OO [0 ]['OfferList']),await best_price (O00O0OOO00000O0OO [1 ]['OfferList'])]#line:39:prices = [await best_price(trade_json[0]['OfferList']), await best_price(trade_json[1]['OfferList'])]
+        if OOOOOOOOOOO00OOO0 [0 ]<OOOOOOOOOOO00OOO0 [1 ]:#line:40:if prices[0] < prices[1]:
+            print (f':pensive: trade #{O0O0000OOO000OOOO+1} is bad you will lose {OOOOOOOOOOO00OOO0[0] - OOOOOOOOOOO00OOO0[1]} robux')#line:41:print(f':pensive: trade #{i+1} is bad you will lose {prices[0] - prices[1]} robux')
+            continue #line:42:continue
+        else :#line:43:else:
+            OO0O00O0OOO00O00O =input (f'\nTrade #{O0O0000OOO000OOOO+1}:\nYou will win {OOOOOOOOOOO00OOO0[0]-OOOOOOOOOOO00OOO0[1]} robux\nYou will give: {parse_items(O00O0OOO00000O0OO[1]["OfferList"])}\nYou will get: {parse_items(O00O0OOO00000O0OO[0]["OfferList"])}\nAccept Trade? (y/n) :')#line:44:msg = input(f'\nTrade #{i+1}:\nYou will win {prices[0]-prices[1]} robux\nYou will give: {parse_items(trade_json[1]["OfferList"])}\nYou will get: {parse_items(trade_json[0]["OfferList"])}\nAccept Trade? (y/n) :')
+            if OO0O00O0OOO00O00O .lower ().startswith ('y'):#line:45:if msg.lower().startswith('y'):
+                await roblox .execute_trade (int (OOOOO000O0O000O00 ['TradeSessionID']),'accept')#line:46:await roblox.execute_trade(int(trade_identify['TradeSessionID']), 'accept')
+            else :#line:47:else:
+                await roblox .execute_trade (int (OOOOO000O0O000O00 ['TradeSessionID']),'decline')#line:48:await roblox.execute_trade(int(trade_identify['TradeSessionID']), 'decline')
+loop =asyncio .get_event_loop ()#line:50:loop = asyncio.get_event_loop()
+loop .run_until_complete (run ())#line:51:loop.run_until_complete(run())
+loop .close ()
